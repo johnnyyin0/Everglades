e3c901819854568b4a66f9a9b8a4caaae31175fd
Ben Tracy <b.rob.tracy@gmail.com>
2023-03-16 18:33:24 -0400

    add route to get metadata

--- server/Controllers/reviewsController.js
+++ server/Controllers/reviewsController.js
@@ -7,21 +7,27 @@ reviewController = {
         res.send(response.data)
       })
       .catch(err => {
-        res.send(err, req.body, 'Failed to fetch reviews')
+        res.send(err, 'Failed to fetch reviews!')
       })
   },
   post: (req, res) => {
     Models.reviews.postReview(req.body)
     .then(response => {
       console.log('Successfully posted review!')
-      res.send(response.data);
+      res.send(response.data)
     })
     .catch(err => {
       console.log(err, 'Failed to post review!')
       res.send(err.data)
     })
+  },
+  getMeta: (req, res) => {
+    Models.reviews.getMeta(req.body)
+    .then(response => {
+      res.send(response.data)
+    })
+    .catch(err => res.send(err, 'Failed to fetch review metadata!'))
   }
-  getMeta(req, res)
 }
 
 
