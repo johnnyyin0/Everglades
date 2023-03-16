d125c618276ae227ad6add4ecc0580ace989d5f0
Ben Tracy <b.rob.tracy@gmail.com>
2023-03-16 18:38:57 -0400

    add comments to describe incoming quest requirements for reviews controllers to work

--- server/Controllers/reviewsController.js
+++ server/Controllers/reviewsController.js
@@ -3,6 +3,7 @@ const Models = require('../Models/Models.js');
 reviewController = {
   get: (req, res) => {
       Models.reviews.getReviews(req.body)
+      //req.body must be an object with at least a product_id key
       .then(response => {
         res.send(response.data)
       })
@@ -12,6 +13,7 @@ reviewController = {
   },
   post: (req, res) => {
     Models.reviews.postReview(req.body)
+    //req.body should include: product_id, rating (1-5), summary, body, recommend(bool), name, email, photos(array), characteristics(obj)
     .then(response => {
       console.log('Successfully posted review!')
       res.send(response.data)
@@ -23,6 +25,7 @@ reviewController = {
   },
   getMeta: (req, res) => {
     Models.reviews.getMeta(req.body)
+    //req.body must be an object with just a product_id key
     .then(response => {
       res.send(response.data)
     })
