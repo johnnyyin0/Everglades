b2c822ff43d9ec50e9a4018afd9c3d63424dc01f
phaeq <johnnyyin0@gmail.com>
2023-03-16 15:47:35 -0400

    Updated questions and answer components on react side

--- server/Controllers/questionController.js
+++ server/Controllers/questionController.js
@@ -1,11 +1,11 @@
 let Models = require('../Models/Models.js');
 
-
 let questionController = {
   get: (req, res) => {
     Models.questions.getQuestions()
+    //should take in a param ie: productId
     .then(data => {
-      console.log('DATA RECEIVED', data)
+      console.log('DATA RECEIVED:', data)
       res.send(data)
     })
     .catch((err) => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 // import Overview from './components/Overview/Overview.jsx'
-// import QuestionsAnswers from './components/QuestionsAnswers/QuestionsAnswers.jsx'
+import QuestionsAnswers from './components/QuestionsAnswers/QuestionsAnswers.jsx'
 // import Reviews from './components/Reviews/Reviews.jsx'
 
 function App() {
 
   return (
-    <div>
-      <p>hello world</p>
-      {/* <Overview />
-      <QuestionsAnswers />
-      <Reviews /> */}
+    <div className="App">
+        {/* <Overview/> */}
+        <QuestionsAnswers/>
+        {/* <Reviews/> */}
     </div>
   )
 }
