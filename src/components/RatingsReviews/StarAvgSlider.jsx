67b91fb0a63a6f05e3a2435f871ed6ed627427f6
Ben Tracy <b.rob.tracy@gmail.com>
2023-03-17 20:05:45 -0400

    add total for each review to StarAvgSlider

--- src/components/RatingsReviews/StarAvgSlider.jsx
+++ src/components/RatingsReviews/StarAvgSlider.jsx
@@ -1,9 +1,13 @@
 export default function StarAvgSlider({num, avg}) {
 
+  //hardcoded for testing
+  let numReviews = 9;
+
   return (
     <div className="mb-1">
+    <span className="float-right mr-7">{`(${numReviews})`}</span>
     <span className="float-right mr-7">
-      <progress className="progress w-64" value={avg} max="100"></progress>
+      <progress className="progress w-52" value={avg} max="100"></progress>
     </span>
     <span className="btn btn-xs ml-7">{num} star{num === 1 ? ' ' : 's'}</span>
     </div>
