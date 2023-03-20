93cf0e1ad20b25fdab677e01eee056592de58cf2
Ben Tracy <b.rob.tracy@gmail.com>
2023-03-20 09:07:39 -0400

    make avg star sliders green per business doc

--- src/components/RatingsReviews/StarAvgSlider.jsx
+++ src/components/RatingsReviews/StarAvgSlider.jsx
@@ -7,7 +7,7 @@ export default function StarAvgSlider({num, avg}) {
     <div className="mb-1">
     <span className="float-right mr-7">{`(${numReviews})`}</span>
     <span className="float-right mr-7">
-      <progress className="progress w-52" value={avg} max="100"></progress>
+      <progress className="progress progress-success w-52" value={avg} max="100"></progress>
     </span>
     <span className="btn btn-xs ml-7">{num} star{num === 1 ? ' ' : 's'}</span>
     </div>
