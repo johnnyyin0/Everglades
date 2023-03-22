d5cec0f234e9bedf8aad5cb3ecda65073233e613
Kyle Bradford <kyle.h.bradford@gmail.com>
2023-03-21 20:45:27 -0400

    Starting adding zoom functionality

--- src/components/Overview/FullScreen.jsx
+++ src/components/Overview/FullScreen.jsx
@@ -1,8 +1,10 @@
 import {useState} from 'react';
-
+import ReactImageMagnify from 'react-image-magnify';
 const FullScreen = ({setFullScreen, styleSelected}) => {
-
+console.log(styleSelected);
   let [index, setIndex] = useState(0);
+
+  // let [size, setSize] = useState({height: 100, width: 100})
 
   let handleFullScreen = () => {
     setFullScreen(false);
@@ -20,14 +22,31 @@ const FullScreen = ({setFullScreen, styleSelected}) => {
     let newIndex = firstButton ? styleSelected.photos.length - 1 : index - 1;
     setIndex(newIndex);
   }
+
+
   return (
     <>
-    <div className=" w-full m-auto py-16 px-4 flex justify-center">
-      <img src={styleSelected.photos[index].thumbnail_url} className="w-full max-h-[30%] max-w-[30%] rounded-2xl bg-center bg-cover duration-500 flex-1"></img>
-      <div className="absolute top-[50%] left-[30%] rounded-full p-2 bg-black/20">
+    <div className="">
+      {/* <img src={styleSelected.photos[index].thumbnail_url} className="w-full max-h-[40%] max-w-[40%] rounded-2xl bg-center bg-cover duration-500 flex-1"></img> */}
+      <div className="max-w-[500px] max-h-[500px]">
+
+      <ReactImageMagnify {...{
+        smallImage: {
+          alt: styleSelected.name,
+          isFluidWidth: true,
+          src: styleSelected.photos[index].thumbnail_url
+        },
+        largeImage: {
+          src: styleSelected.photos[index].thumbnail_url,
+          width: 500,
+          height: 500
+        }
+      }} />
+      </div>
+      <div className="absolute top-[10%] left-[10%] rounded-full p-2 bg-black/20">
         <button onClick={backButton}>🢀</button>
         </div>
-        <div className="absolute top-[50%] right-[30%] rounded-full p-2 bg-black/20">
+        <div className="absolute top-[10%] right-[10%] rounded-full p-2 bg-black/20">
           <button onClick={nextButton}>🢂</button>
           </div>
           </div>
