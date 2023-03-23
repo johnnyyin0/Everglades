278c9b0253bb47d58de50cc59874462d98a6b9af
phaeq <johnnyyin0@gmail.com>
2023-03-23 13:17:56 -0400

    Index css changes

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import App from './App'
 import NewReviewModal from './components/RatingsReviews/NewReviewModal.jsx'
 import FullSizePhoto from './components/RatingsReviews/FullSizePhoto.jsx'
 import './index.css'
-import AddAnswer from './components/QuestionsAnswers/AddAnswerButton.jsx'
 
 ReactDOM.createRoot(document.getElementById('NewReviewModal')).render(
   <NewReviewModal />
