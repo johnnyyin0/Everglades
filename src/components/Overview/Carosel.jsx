b3095139a4da148f8353186df2892d47c7b71bd3
Kyle Bradford <kyle.h.bradford@gmail.com>
2023-03-24 18:26:00 -0400

    Functioning comparison modal on Carousel

--- src/components/Overview/Carosel.jsx
+++ src/components/Overview/Carosel.jsx
@@ -1,12 +1,32 @@
 import StarsWidget from '../RatingsReviews/StarsWidget.jsx';
 import CaroselProduct from './CaroselProduct.jsx';
-let Carosel = ({relative}) => {
+import CompareModal from './CompareModal.jsx'
+import {useState, useEffect} from 'react';
+import Axios from 'axios';
+
+
+let Carosel = ({relative, currentProduct, styleSelected}) => {
+  let [modalVisable, setModal] = useState(false);
+
+  //state to find which product was clicked on
+  let [clickedProduct, selectClickedProduct] = useState('');
+  let [clickedProductStyle, selectClickedProductStyle] = useState('');
+
+  useEffect(()=> {
+    console.log(clickedProduct);
+    console.log(currentProduct);
+    Axios.get(`http://localhost:3000/product/${clickedProduct.id}/styles`)
+    .then(res => selectClickedProductStyle(res.data.results[0]))
+    .catch(err => console.log(err));
+  }, [clickedProduct])
+
   return (
 
-    <div className="carousel carousel-center rounded-box">
+    <div className="carousel carousel-center rounded-lg h-[300px]">
       {relative.map( product =>
-      <CaroselProduct key={product.style_id} product={product}/>
-  )}
+      <CaroselProduct key={product.style_id} product={product} setModal={setModal} selectClickedProduct={selectClickedProduct}/>
+      )}
+      <CompareModal modalVisable={modalVisable} setModal={setModal} styleSelected={styleSelected} clickedProductStyle={clickedProductStyle} currentProduct={currentProduct} clickedProduct={clickedProduct}/>
 </div>
  );
 }
--- src/components/Overview/style.jsx
+++ src/components/Overview/style.jsx
@@ -8,9 +8,9 @@ const Style = ({style, setPhoto, setSelectedStyle, styleSelected, createSkusArra
   return (
     <>
     {style == styleSelected ?
-    <img src={style.photos[0].thumbnail_url} className="object-fill h-[100px] w-[100px] rounded-full ml-4 border-4 border-blue-600 hover:scale-105 ease-in-out duration-300"></img>
+    <img src={style.photos[0].thumbnail_url} className="object-fill h-[70px] w-[70px] rounded-full ml-4 border-4 border-blue-600 hover:scale-105 ease-in-out duration-300"></img>
       : <div onClick={(e) => styleClick(e)}>
-    <img src={style.photos[0].thumbnail_url} className="object-fill h-[100px] w-[100px] rounded-full ml-4 hover:scale-105 ease-in-out duration-300"></img>
+    <img src={style.photos[0].thumbnail_url} className="object-fill h-[60px] w-[60px] rounded-full ml-4 hover:scale-105 ease-in-out duration-300"></img>
     </div>
     }
     </>
