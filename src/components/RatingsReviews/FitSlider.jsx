5f6371ae88b7dbea3fa1f708e69bcbb557aadef7
Ben Tracy <b.rob.tracy@gmail.com>
2023-03-24 09:25:06 -0400

    add dark mode functionality to sidebar fit sliders

--- src/components/RatingsReviews/FitSlider.jsx
+++ src/components/RatingsReviews/FitSlider.jsx
@@ -12,11 +12,11 @@ export default function FitSlider({ fitChar, charId, charVal }) {
     <div className="pb-5 " key={charId} >
       <div className="pb-2 text-2xl">{fitChar}</div>
       <div className="relative flex content-center">
-          <div className="triangle-slider absolute -mt-2 z-10 text-7xl mask mask-triangle-2 bg-black w-full h-4" style={position}>.</div>
+          <div className="triangle-slider absolute -mt-2 z-10 text-7xl mask mask-triangle-2 bg-black dark:bg-slate-200 w-full h-4" style={position}>.</div>
         <div className="flex justify-evenly w-full">
-          <progress className="progress w-3/12" value="0" max="100"></progress>
-          <progress className="progress w-5/12" value="0" max="100"></progress>
-          <progress className="progress w-3/12" value="0" max="100"></progress>
+          <progress className="progress w-3/12 dark:bg-zinc-600" value="0" max="100"></progress>
+          <progress className="progress w-5/12 dark:bg-zinc-600" value="0" max="100"></progress>
+          <progress className="progress w-3/12 dark:bg-zinc-600" value="0" max="100"></progress>
         </div>
       </div>
         <div className="w-full grid grid-cols-12 pt-2">
