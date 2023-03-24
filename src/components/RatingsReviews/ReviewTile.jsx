13f095c3bf7e58cd0630bdc5da2f1745b1949dd7
Ben Tracy <b.rob.tracy@gmail.com>
2023-03-24 11:38:51 -0400

    fix formatting in review body

--- src/components/RatingsReviews/ReviewTile.jsx
+++ src/components/RatingsReviews/ReviewTile.jsx
@@ -39,24 +39,30 @@ export default function ReviewTile({ review, setPhoto }) {
       </div>
       <div className="px-5 py-5 font-bold text-2xl">{review.summary}</div>
       <div className="px-5 pb-5">
-        <span>{!showMore ? clippedReview + restReview : clippedReview}{showMore &&restReview && '...'}</span>
+        <span>
+          {!showMore ? clippedReview + restReview : clippedReview}{showMore && restReview && '...'}
+        </span>
+        {showMore && <span className="float-right hover:text-cyan-800 cursor-pointer" onClick={handleShowMore}>Show more</span>}
+        {showLess && <span className="float-right hover:text-cyan-800 cursor-pointer" onClick={handleShowLess}>Show less</span>}
+      </div>
+      <div className="px-5">
+        {review.recommend && <div className="flex justify-left w-full">
+          <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 24 24" className="w-min mt-2 dark:fill-slate-200"><path d="M9 22l-10-10.598 2.798-2.859 7.149 7.473 13.144-14.016 2.909 2.806z" /></svg>
+          <span className="pl-3 mt-1 text-s pb-5">I recommend this product</span></div>}
+      </div>
       <div>
-        {showMore && <span className="float-right h-min hover:text-cyan-800 cursor-pointer" onClick={handleShowMore}>Show more</span>}
-        {showLess && <span className="float-right h-min hover:text-cyan-800 cursor-pointer" onClick={handleShowLess}>Show less</span>}
+        {review.response && <div className="pb-5">
+          <div className="ml-5 mr-5 bg-slate-200 dark:bg-zinc-600 ">
+            <div className="px-3 py-3 font-bold">Response from the seller:</div>
+            <div className="pl-3 pb-3">{review.response}</div>
+          </div>
+        </div>}
       </div>
+      <div>
+        {review.photos.length > 0 && <div className="pb-5 px-5 carousel">
+          {review.photos.map(photo => <CarouselPhoto src={photo.url} setPhoto={setPhoto} key={photo.id} />)}
+        </div>}
       </div>
-      <div className="px-5">{review.recommend && <div className="flex justify-left w-full">
-        <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 24 24" className="w-min mt-2 dark:fill-slate-200"><path d="M9 22l-10-10.598 2.798-2.859 7.149 7.473 13.144-14.016 2.909 2.806z" /></svg>
-        <span className="pl-3 mt-1 text-s pb-5">I recommend this product</span></div>}</div>
-      <div>{review.response && <div className="pb-5">
-        <div className="ml-5 mr-5 bg-slate-200 dark:bg-zinc-600 ">
-          <div className="px-3 py-3 font-bold">Response from the seller:</div>
-          <div className="pl-3 pb-3">{review.response}</div>
-        </div>
-      </div>}</div>
-      <div>{review.photos.length > 0 && <div className="pb-5 px-5 carousel">
-        {review.photos.map(photo => <CarouselPhoto src={photo.url} setPhoto={setPhoto} key={photo.id} />)}
-      </div>}</div>
       <div className="px-5 pb-5">
         <small>
           Helpful?{ }
