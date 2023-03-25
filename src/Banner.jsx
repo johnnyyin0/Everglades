27c74d1f1509816e438ac636ab0d17e9247ae2a3
phaeq <johnnyyin0@gmail.com>
2023-03-24 20:38:59 -0400

    more changes to the banner

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -10,20 +10,21 @@ const Banner = () => {
   return (
     <>
       <div style={{position: 'fixed', top: 0, left: 0, right: 0, display: 'flex', alignItems: 'center', justifyContent: 'space-between', background: '#213547', padding: '20px',  zIndex: 1, }}>
-        <div style={{fontWeight: 'bold', fontSize: '30px', color: 'white', display: 'flex', alignItems: 'center',}}>
+        <div style={{fontWeight: 'bold', fontSize: '30px', color: 'white', display: 'flex', alignItems: 'center', cursor: 'pointer'}}>
             EVERGLADES
-          <img src="../gator_icon.jpeg" alt="Everglades logo" style={{width: '50px', height: '50px', borderRadius: '50%', marginLeft: '10px',}}/>
-        </div>
+          <img src="../icons/gator_icon.jpeg" alt="Everglades logo" style={{width: '50px', height: '50px', borderRadius: '50%', marginLeft: '10px',}}/>
+          </div>
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <input type="text" value={searchText} onChange={handleSearch} placeholder="Search"
-            style={{padding: '5px', borderRadius: '0', border: 'none', marginRight: '10px',}}
+          <input type="text" value={searchText} onChange={handleSearch} placeholder=" Search"
+            style={{padding: '5px', borderRadius: '0', border: 'none', height: '40px', backgroundColor: 'white'}}
           />
-          <button style={{ padding: '5px', borderRadius: '5px', border: 'none', backgroundColor: 'white', color: 'grey',}}>
-            Search
+          <button style={{ padding: '5px', borderRadius: '0px', border: 'none', backgroundColor: 'white', color: 'grey',}}>
+          <img src="../icons/search_icon.jpg" alt ="search icon" style={{width: '30px', height: '30px', background: 'white'}}></img>
           </button>
+          <img src="../icons/cart.png" alt="cart icon" style={{width: '30px', height: '30px', marginLeft: '20px', cursor:'pointer',}}></img>
         </div>
       </div>
-      <div style={{position: 'relative', top: '90px', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#f2f2f2', padding: '10px', marginBottom: '50px',}}>
+      <div style={{position: 'relative', top: '90px', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#f2f2f2', padding: '10px', marginBottom: '60px',}}>
         <div>
           <b>
             SIGN UP NOW AND GET 20% OFF + FREE SHIPPING ON YOUR FIRST ORDER!{' '}
