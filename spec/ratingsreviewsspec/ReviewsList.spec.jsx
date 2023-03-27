9764e4319e8f18a6d71e1652d3a721787b287ed2
Ben Tracy <b.rob.tracy@gmail.com>
2023-03-27 12:29:58 -0400

    add skeleton for sidebar test suite

--- spec/ratingsreviewsspec/ReviewsList.spec.jsx
+++ spec/ratingsreviewsspec/ReviewsList.spec.jsx
@@ -1,6 +1,6 @@
 import { assert, expect, it } from 'vitest'
 import {render, screen, fireEvent} from '@testing-library/react'
-import SampleReviewData from './SampleReviewData.js'
+import sampleReviewData from './sampleReviewData.js'
 import ReviewsList from '../../src/components/RatingsReviews/ReviewsList.jsx'
 
 //requires id, setPhoto, shownReviews, setShownReviews, allReviews, setAllReviews, sort, setSort
@@ -11,14 +11,14 @@ describe('ReviewsList', () => {
   const props = {}
   props.id = 37311;
   props.setPhoto = vi.fn();
-  props.shownReviews = SampleReviewData.results.slice(0, 2)
+  props.shownReviews = sampleReviewData.results.slice(0, 2)
   props.setShownReviews = vi.fn();
-  props.allReviews = SampleReviewData.results
+  props.allReviews = sampleReviewData.results
   props.setAllReviews = vi.fn();
   props.sort = 'relevant';
   props.setSort = vi.fn();
 
-  it('should render to the DOM', () => {
+  it('Should render to the DOM', () => {
 
     render(<ReviewsList {...props} />);
     expect(screen.getByTitle('reviews-list')).toBeInTheDocument()
