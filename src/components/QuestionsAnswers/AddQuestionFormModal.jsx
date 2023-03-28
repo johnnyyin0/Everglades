e3a2660f47dfbe538dcb2fc6742ca08c31301f20
phaeq <johnnyyin0@gmail.com>
2023-03-28 18:46:18 -0400

    Fixed bug on add question

--- src/components/QuestionsAnswers/AddQuestionFormModal.jsx
+++ src/components/QuestionsAnswers/AddQuestionFormModal.jsx
@@ -29,61 +29,59 @@ const AddQuestionFormModal = ({ closeModal, productName, productId, getQuestions
         <h2><b>PRODUCT: {productName}</b></h2>
         <h2><b>* Indicates a required field</b></h2>
         <form onSubmit={handleSubmit}>
-            <label className='label' htmlFor='nickname'>
-              <span className='label-text'>
-                Nickname*
-              </span>
-            </label>
-            <input
-              type='text'
-              placeholder='Example: jackson11!'
-              className='input input-bordered w-full max-w-xs'
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              id='nickname'
-              maxLength={60}
-              required
-            />
-            <div>
-              <i>
-               <small>*For privacy reasons, do not use your full name or email address!</small> 
-              </i>
-            </div>
-            <label className='label' htmlFor='email'>
-              <span className='label-text'>Your Email*</span>
-            </label>
-            <input
-              type='email'
-              placeholder='Example: jackson@example.com'
-              className='input input-bordered w-full max-w-xs'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              id='email'
-              maxLength={60}
-              required
-            />
-            <div>
-              <i>
-                <small>*For authentication reasons, you will not be emailed!</small>
-              </i>
-            </div>
-            <label className='label' htmlFor='question'>
-              <span className='label-text'>Your Question*</span>
-            </label>
-            <textarea
-              placeholder='Type your question here...'
-              className='input input-bordered w-full max-w-xs'
-              rows='3'
-              value={question}
-              onChange={(e) => setQuestion(e.target.value)}
-              id='question'
-              maxLength={1000}
-              required
-            />
-        </form>
-        <button className='btn' style={{ marginTop: '20px' }} type='submit'>
-            SUBMIT
-          </button>
+  <label className='label' htmlFor='nickname'>
+    <span className='label-text'>Nickname*</span>
+  </label>
+  <input
+    type='text'
+    placeholder='Example: jackson11!'
+    className='input input-bordered w-full max-w-xs'
+    value={name}
+    onChange={(e) => setName(e.target.value)}
+    id='nickname'
+    maxLength={60}
+    required
+  />
+  <div>
+    <i>
+      <small>*For privacy reasons, do not use your full name or email address!</small> 
+    </i>
+  </div>
+  <label className='label' htmlFor='email'>
+    <span className='label-text'>Your Email*</span>
+  </label>
+  <input
+    type='email'
+    placeholder='Example: jackson@example.com'
+    className='input input-bordered w-full max-w-xs'
+    value={email}
+    onChange={(e) => setEmail(e.target.value)}
+    id='email'
+    maxLength={60}
+    required
+  />
+  <div>
+    <i>
+      <small>*For authentication reasons, you will not be emailed!</small>
+    </i>
+  </div>
+  <label className='label' htmlFor='question'>
+    <span className='label-text'>Your Question*</span>
+  </label>
+  <textarea
+    placeholder='Type your question here...'
+    className='input input-bordered w-full max-w-xs'
+    rows='3'
+    value={question}
+    onChange={(e) => setQuestion(e.target.value)}
+    id='question'
+    maxLength={1000}
+    required
+  />
+  <button className='btn' style={{ marginTop: '20px' }} type='submit'>
+    SUBMIT
+  </button>
+</form>
       </div>
     </div>
   );
