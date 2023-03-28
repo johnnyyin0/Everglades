e891a5023a239fa39ef2552cbdfc32bcfcddbf04
EC2 Default User <ec2-user@ip-172-31-9-253.us-east-2.compute.internal>
2023-03-28 19:53:43 +0000

    Fixes to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+mport { defineConfig } from 'vite'
 import viteCompression from 'vite-plugin-compression'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
@@ -8,15 +8,13 @@ import path from 'path'
 dotenv.config()
 
 export default defineConfig({
-<<<<<<< HEAD
-	  plugins: [react()],
+	  plugins: [react(), viteCompression()],
 	  test: {
 		      globals: true,
 		      environment: 'jsdom',
 		      setupFiles: './spec/setup.js'
 		    },
 	  server: {
-		      host:true,
 		      proxy: {
 			            "/api" : {
 					            target: `http://ec2-3-137-181-202.us-east-2.compute.amazonaws.com:${process.env.PORT}`,
@@ -26,23 +24,5 @@ export default defineConfig({
 					          }
 			          }
 		    }
-=======
-  plugins: [react(), viteCompression()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './spec/setup.js'
-  },
-  server: {
-    proxy: {
-      "/api" : {
-        target: `http://localhost:${process.env.PORT}`,
-        changeOrigin: true,
-        secure: false,
-        rewrite: ( path ) => path.replace(/^\/api/, '')
-      }
-    }
-  }
->>>>>>> 4223a2c9feb445a4185894af3d24d67ff8be635a
 })
 
